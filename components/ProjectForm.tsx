33f60a31c8171670a4ccb2453f5274c38e93ba84
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-18 11:25:56 -0700

    Created a custom menu that has category items available for projects updated state in form component

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { SessionInterface } from "@/common.types";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import Image from "next/image";
 import FormField from "./FormField";
+import { categoryFilters } from "@/constants";
+import CustomMenu from "./CustomMenu";
 
 type Props = {
   type: string;
@@ -10,13 +12,25 @@ type Props = {
 };
 
 const ProjectForm = ({ type, session }: Props) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setform] = useState({
+    title: "",
+    description: "",
+    image: "",
+    liveSitUrl: "",
+    githubUrl: "",
+    category: "",
+  });
+
   const handleFormSubmit = (e: React.FormEvent) => {};
   const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
-  const handleStateChange = (fieldName: string, value: string) => {};
-  const form = {
-    title: "",
-    image: "",
+  const handleStateChange = (fieldName: string, value: string) => {
+    setform((prevState) => ({
+      ...prevState,
+      [fieldName]: value,
+    }));
   };
+
   return (
     <form onSubmit={handleFormSubmit} className="flexStart form">
       <div className="flexStart form_image-container">
@@ -46,6 +60,35 @@ const ProjectForm = ({ type, session }: Props) => {
         placeholder="CodeJournal"
         setState={(value) => handleStateChange("title", value)}
       />
+      <FormField
+        title="Description"
+        state={form.description}
+        placeholder="Showcase beautiful projects."
+        setState={(value) => handleStateChange("description", value)}
+      />
+      <FormField
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="https://brandonmoy.com"
+        setState={(value) => handleStateChange("liveSiteUrl", value)}
+      />
+      <FormField
+        type="url"
+        title="GitHub Url"
+        state={form.githubURL}
+        placeholder="https://github.com/brandon-moy"
+        setState={(value) => handleStateChange("githubUrl", value)}
+      />
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange("category", value)}
+      />
+      <div className="flexStart w-full">
+        <button>Create</button>
+      </div>
     </form>
   );
 };
