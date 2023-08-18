2caaafca43ab354734add88bb44afbeb3cd343e5
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-18 13:35:59 -0700

    Completed setting up project page, layout started to create Related Projects section at bottom of page

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,7 @@ import { ProjectForm } from "@/common.types";
 import {
   createProjectMutation,
   createUserMutation,
+  getProjectByIdQuery,
   getUserQuery,
   projectsQuery,
 } from "@/graphql";
@@ -97,4 +98,9 @@ export const fetchAllProjects = async (
 ) => {
   client.setHeader("x-api-key", apiKey);
   return makeGraphQLRequest(projectsQuery, { category, endcursor });
+};
+
+export const getProjectDetails = (id: string) => {
+  client.setHeader("x-api-key", apiKey);
+  return makeGraphQLRequest(getProjectByIdQuery, { id });
 };
