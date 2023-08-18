c0e985a020ea6393c8c0a3395c43f23f54d6d275
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-18 13:29:41 -0700

    Set up request, renamed misnamed assets, created component to render projects on home page

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+type Props = {
+  id: string;
+  image: string;
+  title: string;
+  name: string;
+  avatarUrl: string;
+  userId: string;
+};
+
+const ProjectCard = ({ id, image, title, name, avatarUrl, userId }: Props) => {
+  return (
+    <div className="flexCenter flex-col rounded-2xl drop-shadow-card">
+      <Link
+        href={`/project/${id}`}
+        className="flexCenter group relative w-full h-full"
+      >
+        <Image
+          src={image}
+          className="w-full h-full object-cover rounded-2xl"
+          width={414}
+          height={314}
+          alt="Project Image"
+        />
+        <div className="hidden group-hover:flex profile_card-title">
+          <p className="w-full">{title}</p>
+        </div>
+      </Link>
+      <div className="flexBetween w-full px-2 mt-3 font-semibold text-sm">
+        <Link href={`/profile/${userId}`}>
+          <div className="flexCenter gap-2">
+            <Image
+              src={avatarUrl}
+              width={24}
+              height={24}
+              className="rounded-full"
+              alt="Profil Image"
+            />
+            <p>{name}</p>
+          </div>
+        </Link>
+        <div className="flexCenter gap-3">
+          <div className="flexCenter gap-2">
+            <Image src="/heart.svg" width={13} height={12} alt="Heart" />
+            <p className="text-sm">500 likes</p>
+          </div>
+          <div className="flexCenter gap-2">
+            <Image src="/eye.svg" width={13} height={12} alt="Views" />
+            <p className="text-sm">5k</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,12 +1,12 @@
-import { g, auth, config } from "@grafbase/sdk";
+import { g, config, auth } from "@grafbase/sdk";
 
 // @ts-ignore
 const User = g
   .model("User", {
-    name: g.string().length({ min: 2, max: 20 }),
+    name: g.string().length({ min: 2, max: 100 }),
     email: g.string().unique(),
     avatarUrl: g.url(),
-    description: g.string().optional(),
+    description: g.string().length({ min: 2, max: 1000 }).optional(),
     githubUrl: g.url().optional(),
     linkedinUrl: g.url().optional(),
     projects: g
@@ -14,7 +14,9 @@ const User = g
       .list()
       .optional(),
   })
-  .auth((rules) => rules.public().read());
+  .auth((rules) => {
+    rules.public().read();
+  });
 
 // @ts-ignore
 const Project = g
@@ -28,7 +30,8 @@ const Project = g
     createdBy: g.relation(() => User),
   })
   .auth((rules) => {
-    rules.public().read(), rules.private().create().delete().update();
+    rules.public().read();
+    rules.private().create().delete().update();
   });
 
 const jwt = auth.JWT({
