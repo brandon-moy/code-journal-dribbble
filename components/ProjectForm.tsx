83883d559e159a864534ea66b93c14ae422e9b67
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-08-18 11:10:05 -0700

    Created reuseable FormField component to create form inputs

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,5 +1,53 @@
-const ProjectForm = () => {
-  return <div>ProjectForm</div>;
+"use client";
+import { SessionInterface } from "@/common.types";
+import { ChangeEvent } from "react";
+import Image from "next/image";
+import FormField from "./FormField";
+
+type Props = {
+  type: string;
+  session: SessionInterface;
+};
+
+const ProjectForm = ({ type, session }: Props) => {
+  const handleFormSubmit = (e: React.FormEvent) => {};
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
+  const handleStateChange = (fieldName: string, value: string) => {};
+  const form = {
+    title: "",
+    image: "",
+  };
+  return (
+    <form onSubmit={handleFormSubmit} className="flexStart form">
+      <div className="flexStart form_image-container">
+        <label htmlFor="poster" className="flexCenter form_image-label">
+          {!form.image && "Choose a poster for your project"}
+        </label>
+        <input
+          id="image"
+          type="file"
+          accept="image/*"
+          required={type === "create"}
+          className="form_image-input"
+          onChange={handleChangeImage}
+        />
+        {form.image && (
+          <Image
+            src={form?.image}
+            className="sm:p-10 object-contain z-20"
+            alt="project poster"
+            fill
+          />
+        )}
+      </div>
+      <FormField
+        title="Title"
+        state={form.title}
+        placeholder="CodeJournal"
+        setState={(value) => handleStateChange("title", value)}
+      />
+    </form>
+  );
 };
 
 export default ProjectForm;
